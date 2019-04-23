919fb5df18959b335686d791fc3f634970914489
lambda-ai <be.lambda.ai@gmail.com>
2019-04-23 19:20:01 +0200

    switch to make available on github and clojars

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # clj-rtf-parser
 
-A Clojure library designed to ... well, that part is up to you.
+Tiny clojure wrapper around https://github.com/joniles/rtfparserkit, but copying
+java code into this repo as well, since we may tweak it.
+For now, the unit tests fail.
 
 ## Usage
 
-FIXME
+Deployed to clojars.
 
 ## License
 
-Copyright © 2017 FIXME
+Copyright © 2019 lambda-ai
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clj-rtf-parser "0.1.0-SNAPSHOT"
+(defproject clj-rtf-parser "0.1.0"
   :description "Tiny clojure wrapper and copy of the code from https://github.com/joniles/rtfparserkit"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/lambda-ai/clj-rtf-parser"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.sun.xml.txw2/txw2 "20110809"]
                  [junit/junit "4.12"]]
   :plugins [[lein-junit "1.1.8"]]
